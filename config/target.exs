71573f9b3633abf54803dcb56adf5114f5a6e3c0
Victoria Madrid <victoria@wyeworks.com>
2020-03-01 17:57:41 -0300

    Setup WiFi networking

--- config/target.exs
+++ config/target.exs
@@ -44,12 +44,29 @@ config :nerves_firmware_ssh,
 # Only enable this for prod if you understand the risks.
 node_name = if Mix.env() != :prod, do: "blinky"
 
+# Wireless settings
 config :nerves_init_gadget,
-  ifname: "usb0",
-  address_method: :dhcpd,
   mdns_domain: "nerves.local",
   node_name: node_name,
-  node_host: :mdns_domain
+  node_host: :mdns_domain,
+  ifname: "wlan0",
+  address_method: :dhcp
+
+key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
+
+config :nerves_network, :default,
+  wlan0: [
+    networks: [
+      [
+        ssid: System.get_env("NERVES_NETWORK_SSID"),
+        psk: System.get_env("NERVES_NETWORK_PSK"),
+        key_mgmt: String.to_atom(key_mgmt)
+      ]
+    ]
+  ],
+  eth0: [
+    ipv4_address_method: :dhcp
+  ]
 
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
